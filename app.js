4cd26ec2ca722b061b48c4dc0e2ef85dbb84c65b
gajendra-github <gajendra.github@gmail.com>
2023-02-20 00:06:03 -0500

    added chalk : chalk allows to set colors on message

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 //Not ES6 module
 //commonJS
 const express = require('express');
+const chalk = require('chalk');
+
+
+
+
+
 //instance of express and execute
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Building Application With NodeJs and Express...');
-})
+});
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000');
-})
+    console.log('Listening on port 3000 ' + chalk.green('3000'));
+});
+
+
+
+
 
