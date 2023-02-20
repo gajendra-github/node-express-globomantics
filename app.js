acb126e7ac3a5ddca48e783029c9cbf71f882fce
gajendra-github <gajendra.github@gmail.com>
2023-02-20 08:54:02 -0500

    added index.html and configure middleware to handle static files using express.static helper function

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 
+//no need to install path, comes out of the box as part of nodejs
+const path = require('path');
+
+
+
 //instance of express and execute
 const app = express();
 
 //app.use(morgan('combined'));      //logs maximum info..
 app.use(morgan('tiny'));            //logs minimum info..
 
+//express.static helper function sets up a middleware to handle static files
+//locate static files in the file system with path.join()
+//__dirname ~ variable name comes bundled into node ~ 
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/', (req, res) => {
     res.send('Building Application With NodeJs and Express...');
