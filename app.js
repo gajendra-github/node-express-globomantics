3f31e22bbc9f8c45485279eed196b7a8a0de1935
gajendra-github <gajendra.github@gmail.com>
2023-02-20 08:39:27 -0500

    added debug: A tiny javascript debugging utility and note.txt file

--- app.js
+++ app.js
@@ -2,20 +2,23 @@
 //commonJS
 const express = require('express');
 const chalk = require('chalk');
-
-
-
-
+const debug = require('debug')('app');
+const morgan = require('morgan');
 
 //instance of express and execute
 const app = express();
+
+//app.use(morgan('combined'));      //logs maximum info..
+app.use(morgan('tiny'));            //logs minimum info..
+
 
 app.get('/', (req, res) => {
     res.send('Building Application With NodeJs and Express...');
 });
 
 app.listen(3000, () => {
-    console.log(`listening on port ${chalk.green('3000')}`);
+    //console.log(`listening on port ${chalk.green('3000')}`);
+    debug(`listening on port ${chalk.green('3000')}`);
 });
 
 
