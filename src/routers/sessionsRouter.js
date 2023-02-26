6831ceff8a2bcad59c2c07a6ec20fc66faebf847
gajendra-github <gajendra.github@gmail.com>
2023-02-26 16:47:37 -0500

    update

--- src/routers/sessionsRouter.js
+++ src/routers/sessionsRouter.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const debug = require('debug')('app:sessionRouter');
 const { MongoClient, ObjectId } = require('mongodb');
-
-
-//const sessions = require('../data/metro-inventory.json');
-//const sessions = require('../data/sessions.json');
 const sessionsRouter = express.Router();
 
-
+const uri = process.env.MON
 
 
 sessionsRouter.route('/').get((req, res) => {
-    const url = 'mongodb+srv://dbUser:m8bYPccxSv2rfP71@cluster0.n6wmrai.mongodb.net?retryWrites=true&w=majority';
+
+
+    //const url = 'mongodb+srv://dbUser:m8bYPccxSv2rfP71@cluster0.n6wmrai.mongodb.net?retryWrites=true&w=majority';
+    const uri = process.env.MONGO_URI;
+
     const dbName = 'globomantics';
 
     (async function mongodb() {
@@ -19,7 +19,7 @@ sessionsRouter.route('/').get((req, res) => {
         let client;
 
         try {
-            client = await MongoClient.connect(url, { connectTimeoutMS: 600000 });
+            client = await MongoClient.connect(uri);
             res.send('Hello');
         } catch (error) {
             console.log(error.stack);
@@ -29,62 +29,6 @@ sessionsRouter.route('/').get((req, res) => {
 });
 
 
-// sessionsRouter.route('/').get((req, res) => {
-//     const url = 'mongodb+srv://dbUser:m8bYPccxSv2rfP71@cluster0.n6wmrai.mongodb.net?retryWrites=true&w=majority';
-
-//     const dbName = 'globomantics';
-//     console.log('1');
-//     (async function mongo() {
-//         let client;
-
-//         try {
-//             client = await MongoClient.connect(url);
-//             debug('Connected to the mongo DB');
-//             console.log('Connected to the mongo DB');
-//             const db = client.db(dbName);
-
-//             const sessions = await db.collection('sessions').find().toArray();
-//             res.render('sessions', { sessions });
-
-//         } catch (error) {
-//             console.log(error.stack);
-//             debug(error.stack);
-//         }
-//     }());
-// })
-
-// sessionsRouter.route('/:id').get((req, res) => {
-//     const id = req.params.id;
-
-//     const url = 'mongodb+srv://dbUser:m8bYPccxSv2rfP71@cluster0.n6wmrai.mongodb.net?retryWrites=true&w=majority';
-
-//     const dbName = 'globomantics';
-
-//     (async function mongo() {
-//         let client;
-
-//         try {
-//             client = await MongoClient.connect(url);
-//             debug('Connected to the mongo DB');
-//             // console.log('Connected to the mongo DB');
-//             const db = client.db(dbName);
-
-//             const session = await db.collection('sessions').findOne({ _id: new ObjectId(id) });
-
-//             res.render('session', {
-//                 session,
-//             })
-//         } catch (error) {
-//             console.log(error.stack);
-//             debug(error.stack);
-//         }
-//     }());
-
-
-
-
-
-// });
 
 module.exports = sessionsRouter;
 
